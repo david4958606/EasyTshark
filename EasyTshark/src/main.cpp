cb2ef22c98cd0271bbaf42040b7c87a6137291e0
David Wang <wangyudong2002@outlook.com>
2025-05-26 03:19:04 +0800

    L9 main中打印相关提示

--- EasyTshark/src/main.cpp
+++ EasyTshark/src/main.cpp
@@ -19,54 +19,18 @@ int main(int argc, char* argv[])
     std::filesystem::path cwd = std::filesystem::current_path();
     TsharkManager         tsharkManager(cwd.string());
 
-    // tsharkManager.StartMonitorAdaptersFlowTrend();
-    // std::this_thread::sleep_for(std::chrono::seconds(10));
-    // std::map<std::string, std::map<long, long>> trendData;
-    // tsharkManager.GetAdaptersFlowTrendData(trendData);
-    // tsharkManager.StopMonitorAdaptersFlowTrend();
-    //
-    // // 把获取到的数据打印输出
-    // rapidjson::Document                 resDoc;
-    // rapidjson::Document::AllocatorType& allocator = resDoc.GetAllocator();
-    // resDoc.SetObject();
-    // rapidjson::Value dataObject(rapidjson::kObjectType);
-    // for (const auto& adaptorItem : trendData)
-    // {
-    //     rapidjson::Value adaptorDataList(rapidjson::kArrayType);
-    //     for (const auto& timeItem : adaptorItem.second)
-    //     {
-    //         rapidjson::Value timeObj(rapidjson::kObjectType);
-    //         timeObj.AddMember("time", (unsigned int)timeItem.first, allocator);
-    //         timeObj.AddMember("bytes", (unsigned int)timeItem.second, allocator);
-    //         adaptorDataList.PushBack(timeObj, allocator);
-    //     }
-    //
-    //     dataObject.AddMember(rapidjson::StringRef(adaptorItem.first.c_str()), adaptorDataList, allocator);
-    // }
-    //
-    // resDoc.AddMember("data", dataObject, allocator);
-    //
-    // // 序列化为 JSON 字符串
-    // rapidjson::StringBuffer                    buffer;
-    // rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-    // resDoc.Accept(writer);
-    //
-    // LOG_F(INFO, "Network Adaptor Flow: %s", buffer.GetString());
-
-    tsharkManager.StartCapture("WLAN");
-
-    std::string input;
-    while (true)
-    {
-        std::cout << "请输入q退出抓包: ";
-        std::cin >> input;
-        if (input == "q")
-        {
-            tsharkManager.StopCapture();
-            break;
-        }
-    }
+    std::string pcapPath;
+    std::cout << "Please enter PCAP file path: ";
+    std::cin >> pcapPath;
+    tsharkManager.ReadPcap(pcapPath);
     tsharkManager.PrintAllPackets();
+
+    uint32_t    frameNumber;
+    std::string result;
+    std::cout << "Please enter frame number: ";
+    std::cin >> frameNumber;
+    tsharkManager.GetPackageDetailInfo(frameNumber, result);
+    std::cout << result << std::endl;
 }
 
 void InitIp2RegionUtil()
